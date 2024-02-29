b9897bfae30dc1add318693f2222c714196d87d4
ramicorrea21 <ramicorrea021@gmail.com>
2024-02-29 19:57:09 -0300

    analitycs

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import Navbar from "./components/navbar";
 import WspButton from "./components/wsp";
+import { Analytics } from "@vercel/analytics/react"
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -21,6 +22,7 @@ export default function RootLayout({
       <body className={inter.className}>
         <Navbar/>
         {children}
+        <Analytics/>
         <WspButton/>
         </body>
     </html>
